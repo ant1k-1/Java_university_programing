d428fdf53c12fc153c338260ecd16c5c252cefbc
Anton1902 <anton.mail1902@yandex.ru>
2022-09-02 17:10:56 +0300

    Test commit

---
+++ src/practice_1/Ball.java
@@ -0,0 +1,41 @@
+package practice_1;
+import java.lang.*;
+
+public class Ball {
+    private float cost;
+    private String typeOfBall;
+
+    public Ball(String typeOfBall, float cost){
+        this.typeOfBall = typeOfBall;
+        this.cost = cost;
+    }
+
+    public Ball(String typeOfBall){
+        this.typeOfBall = typeOfBall;
+    }
+
+    public Ball(){
+        typeOfBall = "Ball";
+        cost = 0;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public void setTypeOfBall(String typeOfBall) {
+        this.typeOfBall = typeOfBall;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public String getTypeOfBall() {
+        return typeOfBall;
+    }
+
+    public String toString() {
+        return "This ball is for " + this.getTypeOfBall() + " and its cost is " + this.getCost() + "$";
+    }
+}
---
+++ src/practice_2/TestAuthor.java
@@ -0,0 +1,8 @@
+package practice_2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author1 = new Author("Ivanov Ivan", "ivanov.i.i@gmail.com", 'M');
+        System.out.println(author1);
+    }
+}
