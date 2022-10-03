07a1c0b7ed867b3d8fe8d0bba11695dfb9a6524b
Anton1902 <anton.mail1902@yandex.ru>
2022-10-03 21:24:57 +0300

    old practics

---
+++ src/Lab_4/Book.java
@@ -0,0 +1,16 @@
+package Lab_4;
+
+public class Book implements Priceable{
+    private double cost;
+    private String title;
+
+    public Book(double cost, String title) {
+        this.cost = cost;
+        this.title = title;
+    }
+
+    @Override
+    public double getPrice() {
+        return cost;
+    }
+}
