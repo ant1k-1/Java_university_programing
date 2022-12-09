e2ebd3cc820b6e8bd4634ed3cc9a234639dc1856
Anton1902 <anton.mail1902@yandex.ru>
2022-12-09 09:59:44 +0300

    commit

--- src/new_pracs/practice_13/task4/Main.java
+++ src/new_pracs/practice_13/task4/Main.java
@@ -1,5 +1,15 @@
 package new_pracs.practice_13.task4;
 
+//Задание 4. (25%) Реализуйте класс Shirt: Метод toString() выводит
+//объяснение и значение полей построчно.
+//Дан также строковый массив: shirts[0] = "S001,Black Polo Shirt,Black,XL";
+//shirts[1] = "S002,Black Polo Shirt,Black,L"; shirts[2] = "S003,Blue Polo
+//Shirt,Blue,XL"; shirts[3] = "S004,Blue Polo Shirt,Blue,M"; shirts[4] = "S005,Tan Polo
+//Shirt,Tan,XL"; shirts[5] = "S006,Black T-Shirt,Black,XL"; shirts[6] = "S007,White TShirt,White,XL"; shirts[7] = "S008,White T-Shirt,White,L"; shirts[8] = "S009,Green
+//T-Shirt,Green,S"; shirts[9] = "S010,Orange T-Shirt,Orange,S"; shirts[10] =
+//"S011,Maroon Polo Shirt,Maroon,S";
+//Преобразуйте строковый массив в массив класса Shirt и выведите его на
+//консоль.
 public class Main {
     public static void main(String[] args) {
         String[] shirts = new String[11];
